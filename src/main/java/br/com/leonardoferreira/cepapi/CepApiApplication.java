bd0391c90d36fd47a82f6c6d12c2b6f5a4aab5ae
Leonardo Ferreira <leonardo.ferreira@s2it.com.br>
2017-10-05 15:46:16 -0300

    commit initial

---
+++ src/main/java/br/com/leonardoferreira/cepapi/CepApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.leonardoferreira.cepapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepApiApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/leonardoferreira/cepapi/CepApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.leonardoferreira.cepapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CepApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
