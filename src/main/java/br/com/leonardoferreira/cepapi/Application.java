8f94279fe9a8d6f4dde92aa973190d0ee9bd115c
Leonardo Ferreira <leonardo.ferreira@s2it.com.br>
2017-10-05 15:47:50 -0300

    rename and dependencies

--- src/main/java/br/com/leonardoferreira/cepapi/Application.java
+++ src/main/java/br/com/leonardoferreira/cepapi/Application.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CepApiApplication {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(CepApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
